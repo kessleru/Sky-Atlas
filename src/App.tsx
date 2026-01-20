4d49f858d5d997d024a36ca39da4b38077c46013
kessleru <otaviokesslerustra@hotmail.com>
2026-01-19 23:10:58 -0300

    feat: integrate React Query for weather data fetching and add Zod for schema validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import { getWeather } from './api';
+
 function App() {
-  return <></>;
+  const { data } = useQuery({
+    queryKey: ['weather'],
+    queryFn: () => getWeather({ lat: 0, lon: 0 }),
+  });
+
+  return <>{JSON.stringify(data)}</>;
 }
 
 export default App;
---
+++ src/api.tsx
@@ -0,0 +1,9 @@
+import { OneCallSchema } from "./schemas/weatherSchema";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+export async function getWeather({lat, lon}: {lat: number; lon: number}) {
+  const res = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&appid=${API_KEY}`);
+  const data = await res.json();
+  return OneCallSchema.parse(data);
+}
