f4bfcb46dfdeb2bb4fa873b9a6f5cc185a93116b
kessleru <otaviokesslerustra@hotmail.com>
2026-01-26 16:44:18 -0300

    feat: update LocationDropdown to display city options and enhance styling

--- src/components/dropdowns/LocationDropdown.tsx
+++ src/components/dropdowns/LocationDropdown.tsx
@@ -9,14 +9,33 @@ import {
 export default function LocationDropdown() {
   return (
     <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Theme" />
+      <SelectTrigger className="w-[220px]">
+        <SelectValue placeholder="Selecione uma cidade" />
       </SelectTrigger>
-      <SelectContent className='z-1001'>
-        <SelectItem value="light">Light</SelectItem>
-        <SelectItem value="dark">Dark</SelectItem>
-        <SelectItem value="system">System</SelectItem>
+      <SelectContent className="z-[1001] max-h-[300px]">
+        {locations.map((city) => (
+          <SelectItem key={city.value} value={city.value}>
+            {city.label}
+          </SelectItem>
+        ))}
       </SelectContent>
     </Select>
   );
 }
+const locations = [
+  { value: "ituiutaba", label: "Ituiutaba, MG" },
+  { value: "sao-paulo", label: "São Paulo, SP" },
+  { value: "rio-de-janeiro", label: "Rio de Janeiro, RJ" },
+  { value: "belo-horizonte", label: "Belo Horizonte, MG" },
+  { value: "salvador", label: "Salvador, BA" },
+  { value: "curitiba", label: "Curitiba, PR" },
+  { value: "brasilia", label: "Brasília, DF" },
+  { value: "fortaleza", label: "Fortaleza, CE" },
+  { value: "new-york", label: "Nova York, EUA" },
+  { value: "paris", label: "Paris, França" },
+  { value: "london", label: "Londres, Reino Unido" },
+  { value: "tokyo", label: "Tóquio, Japão" },
+  { value: "dubai", label: "Dubai, EAU" },
+  { value: "roma", label: "Roma, Itália" },
+  { value: "lisboa", label: "Lisboa, Portugal" },
+];
